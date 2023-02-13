802475883dadf0f58d1543bbd38296a225f37b4c
alaljanub <71462407+alaljanub@users.noreply.github.com>
2023-02-14 01:12:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
-const contoh = function(string) {
-  let newString = '';
-  for (var i = 0, len = string.length; i < len; i++) {
-    if (string[i] == 'a') {
-      newString += '4';
-    } else if (string[i] == 's') {
-      newString += '5';
-    } else if (string[i] == 'e') {
-      newString += '3';
-    } else if (string[i] == 'o') {
-      newString += '0';
-    } else if (string[i] == 'b') {
-      newString += '8';
-    } else if (string[i] == 'g') {
-      newString += '9';
-    } else if (string[i] == 'i') {
-      newString += '1';
-    }  else {
-      newString += string[i];
-    }
-  }
-  return newString;
+export const sayHello = (name) => {
+    return `Hello ${name}`;
 }
-module.exports =  contoh;
+
+export const sum = (numbers) => {
+    let total = 0;
+    for (const number of numbers) {
+        total += number
+    }
+    return total
+}
